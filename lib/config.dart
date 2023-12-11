3e14c6eff687d432c4308a10af2baecb832fc079
Vladyslav <56268050+Sharfikgod@users.noreply.github.com>
2023-12-11 11:28:17 +0200

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -3,7 +3,7 @@ import 'package:injectable/injectable.dart';
 @singleton
 class AppConfig {
   static const _baseUrl = 'https://joke.api.gkamelo.xyz';
-  static const _apiKey = 'QUtFhHPnlQ5f13LDVpQL3a54XgQzTlCJa1PMSB3o';
+  static const _apiKey = '';
 
   String get baseUrl => _baseUrl;
   String get apiKey => _apiKey;
